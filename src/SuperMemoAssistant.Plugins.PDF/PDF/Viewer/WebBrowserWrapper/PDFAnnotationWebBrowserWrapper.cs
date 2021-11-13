ff15827d7622e5c138758b9fe47e473fe72ed4da
kpence <kpence@tamu.edu>
2021-11-13 11:15:26 -0600

    Configure Windows Registry to make PDF Annotation Web browser utilize the latest version of IE

--- src/SuperMemoAssistant.Plugins.PDF/PDF/Viewer/WebBrowserWrapper/PDFAnnotationWebBrowserWrapper.cs
+++ src/SuperMemoAssistant.Plugins.PDF/PDF/Viewer/WebBrowserWrapper/PDFAnnotationWebBrowserWrapper.cs
@@ -6,6 +6,93 @@ using System.Collections.Specialized;
 
 namespace SuperMemoAssistant.Plugins.PDF.PDF.Viewer.WebBrowserWrapper
 {
+  public class WebBrowserHelper
+  {
+      public static int GetEmbVersion()
+      {
+          int ieVer = GetBrowserVersion();
+
+          if (ieVer > 9)
+              return ieVer * 1000 + 1;
+
+          if (ieVer > 7)
+              return ieVer * 1111;
+
+          return 7000;
+      } // End Function GetEmbVersion
+
+      public static void FixBrowserVersion()
+      {
+          FixBrowserVersion("PluginHost");
+      }
+
+      public static void FixBrowserVersion(string appName)
+      {
+          FixBrowserVersion(appName, GetEmbVersion());
+      } // End Sub FixBrowserVersion
+
+      public static void FixBrowserVersion(string appName, int ieVer)
+      {
+          FixBrowserVersion_Internal("HKEY_LOCAL_MACHINE", appName + ".exe", ieVer);
+          FixBrowserVersion_Internal("HKEY_CURRENT_USER", appName + ".exe", ieVer);
+          FixBrowserVersion_Internal("HKEY_LOCAL_MACHINE", appName + ".vshost.exe", ieVer);
+          FixBrowserVersion_Internal("HKEY_CURRENT_USER", appName + ".vshost.exe", ieVer);
+      } // End Sub FixBrowserVersion 
+
+      private static void FixBrowserVersion_Internal(string root, string appName, int ieVer)
+      {
+          try
+          {
+              //For 64 bit Machine 
+              if (Environment.Is64BitOperatingSystem)
+              {
+                  //MessageBox.Show("is 64"); // TODO NOCHECKIN
+                  Microsoft.Win32.Registry.SetValue(root + @"\Software\Wow6432Node\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", appName, ieVer);
+                  //MessageBox.Show("Success for "+appName + "|"+ieVer.ToString()); // TODO NOCHECKIN
+              }
+              else  //For 32 bit Machine 
+                  Microsoft.Win32.Registry.SetValue(root + @"\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", appName, ieVer);
+
+
+          }
+          catch (Exception exception)
+          {
+            // some config will hit access rights exceptions
+            // this is why we try with both LOCAL_MACHINE and CURRENT_USER
+            //TODO NOCHECKIN (remove this)
+            //MessageBox.Show("calledcalled: "+exception.ToString());
+          }
+      } // End Sub FixBrowserVersion_Internal 
+
+      public static int GetBrowserVersion()
+      {
+          // string strKeyPath = @"HKLM\SOFTWARE\Microsoft\Internet Explorer";
+          string strKeyPath = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Internet Explorer";
+          string[] ls = new string[] { "svcVersion", "svcUpdateVersion", "Version", "W2kVersion" };
+
+          int maxVer = 0;
+          for (int i = 0; i < ls.Length; ++i)
+          {
+              object objVal = Microsoft.Win32.Registry.GetValue(strKeyPath, ls[i], "0");
+              string strVal = System.Convert.ToString(objVal);
+              if (strVal != null)
+              {
+                  int iPos = strVal.IndexOf('.');
+                  if (iPos > 0)
+                      strVal = strVal.Substring(0, iPos);
+
+                  int res = 0;
+                  if (int.TryParse(strVal, out res))
+                      maxVer = Math.Max(maxVer, res);
+              } // End if (strVal != null)
+
+          } // Next i
+
+          //MessageBox.Show("FixBrowserVersion version is" + maxVer.ToString()); // TODO NOCHECKIN
+          return maxVer;
+      } // End Function GetBrowserVersion 
+  }
+
   public class PDFAnnotationWebBrowserWrapper
   {
     private IPDFViewer PDFViewer { get; set; }
@@ -13,6 +100,7 @@ namespace SuperMemoAssistant.Plugins.PDF.PDF.Viewer.WebBrowserWrapper
     public PDFAnnotationWebBrowserWrapper(System.Windows.Forms.Integration.WindowsFormsHost wfHost, IPDFViewer pdfViewer)
     {
       PDFViewer = pdfViewer;
+      WebBrowserHelper.FixBrowserVersion();
       AnnotationWebBrowser = new System.Windows.Forms.WebBrowser();
       wfHost.Child = AnnotationWebBrowser;
       AnnotationWebBrowser.DocumentCompleted += WebBrowserLoadCompletedEventHandler;
